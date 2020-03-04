4ce479967a41b7b57a21917a030490622e60126b
Eric Boehs <eric@boehs.com>
2020-03-04 09:05:31 -0600

    Use App as environment loader

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'bundler/setup'
 require 'active_record'
 require 'sinatra'
 require './lib/hacker_news_api/client'
--- worker.rb
+++ worker.rb
@@ -1,22 +1,12 @@
 # frozen_string_literal: true
 
-require 'active_record'
-require './lib/hacker_news_api/client'
-require './models/item'
+require './app'
 
 # The Worker
 class Worker
-  MIN_SCORES = [0, 50, 100, 200, 300, 400, 500, 750, 1000].freeze
-
-  def self.boot
-    url = ENV['DATABASE_URL'] || 'postgres://localhost/hackinews_development'
-    ActiveRecord::Base.establish_connection url
-  end
-
-  def run
+  def self.run
     Item.top_stories
   end
 end
 
-Worker.boot
-Worker.new.run
+Worker.run
